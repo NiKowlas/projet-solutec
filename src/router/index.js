547b284a807406912845c4c3f04dc0fe6f8bad12
Nicolaskow <nicolas.kowalski@etudiant.univ-lille.fr>
2020-03-18 17:59:01 +0100

    Suppression de bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import List from '@/components/List'
 import Helloworld from '@/components/Helloworld'
 
 Vue.use(Router)
@@ -12,15 +11,5 @@ export default new Router({
       name: 'Homepage',
       component: Helloworld
     }
-    // {
-    //   name: "login",
-    //   path: "/login",
-    //   component: Login
-    // },
-    // {
-    //   name: "register",
-    //   path: "/register",
-    //   component: Register
-    // },
   ]
 })
