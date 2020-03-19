f5af3877d9542c3499c6abdb83ec6597c0781e84
Nicolaskow <nicolas.kowalski@etudiant.univ-lille.fr>
2020-03-19 13:29:35 +0100

    Ajout d'un carousel manque certaines fonctions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,51 @@
 <template>
   <div class="hello">
-      <list></list>
-    
+      <carousel>
+        <carousel-slide v-for="n in slides" :index="n-1" :key="n">
+          <img :src="'https://picsum.photos/1280/720/?'+n" width="100%">
+        </carousel-slide>
+      </carousel>
+    <!-- <button @click="addSlide">Ajouter un Slide</button>
+    <button @click="removeSlide">Supprimer un Slide</button>
+   -->
+  <list></list>
   </div>
 </template>
 
 <script>
 
 import List from '@/components/List'
+import CarouselSlide from '@/components/carousel/CarouselSlide'
+import Carousel from '@/components/carousel/Carousel'
 
 export default {
   name: 'HelloWorld',
   components: {
-    List
+    List, Carousel, CarouselSlide
   },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
+      slides : 5,
       list: [{
         name: 'Demo',
         completed: true
       }]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        name:'test',
+        completed: false
+      })
+      
+    },
+    addSlide() {
+      this.slides++
+    },
+    removeSlide() {
+      this.slides--
     }
   }
 }
